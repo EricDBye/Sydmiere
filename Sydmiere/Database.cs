9ca4190adf243614563046149e6a451356197213
Eric <ericdbye@gmail.com>
2015-08-05 23:31:29 -0500

    Homepage Start

---
+++ Sydmiere/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Sydmiere
+{
+    public class Database
+    {
+        public static bool SaveNotes(string notes)
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Sydmiere"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SaveNotes", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("@PlayerId", 1));
+            cmd.Parameters.Add(new SqlParameter("@Notes", notes));
+
+            rdr = cmd.ExecuteReader();
+            return true;
+        }
+    }
+}
---
+++ Sydmiere/Home.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Sydmiere.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server">Notes</asp:Label>
+        <asp:TextBox runat="server" ID="txtNotes"></asp:TextBox>
+        <asp:Button runat ="server" onclick="btnSaveClick" Text="Save Note"/>
+    </div>
+    </form>
+</body>
+</html>
